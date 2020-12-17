80b8f3a19090b9a4d7eb8e1d71658d361f851a3e
dengjun <dengjun1124@offcn.com>
2020-12-17 22:18:28 +0800

    增加方法

--- src/main/java/com/offcn/webui/Add.java
+++ src/main/java/com/offcn/webui/Add.java
@@ -4,5 +4,8 @@ public class Add {
     public static void main(String[] args) {
         System.out.println("shijie");
  System.out.println("shijie11111111111111");
+ if (true){
+     System.out.println("meihaoshijie");
+ }
     }
 }
