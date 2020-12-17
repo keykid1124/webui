d0280fe5d96c3ef42dff826dc6c4c4e141c68c0a
keykid1124 <76152238+keykid1124@users.noreply.github.com>
2020-12-17 22:15:26 +0800

    Update Add.java
    增加输出

--- src/main/java/com/offcn/webui/Add.java
+++ src/main/java/com/offcn/webui/Add.java
@@ -3,5 +3,6 @@ package com.offcn.webui;
 public class Add {
     public static void main(String[] args) {
         System.out.println("shijie");
+ System.out.println("shijie11111111111111");
     }
 }
